699a47a2cd32f8849a2b5b3cdde6c444acc5fdf0
pseudonacer <nacereddine.laddaoui@etu.sorbonne-universite.fr>
2023-02-15 12:35:32 +0100

    fixed

--- demo.py
+++ demo.py
@@ -17,54 +17,14 @@ def argcomplete_video(prefix, parsed_args, **kwargs) :
 
 def argcomplete_interval(prefix, parsed_args, **kwargs) :
     """ Argument Completer for intervals """
-    file_ = open(parsed_args.from_json, 'r')
+    file_ = open(parsed_args.from_json, 'r') #
     data  = json.load(file_)[parsed_args.set][parsed_args.speaker]
     key   = list(filter(lambda x : x.split('||')[1] == parsed_args.video, list(data.keys())))[0]
     data = data[key]
     return ( itrv for itrv in data if itrv.startswith(prefix))
 
-
-def download_video(url, filename):
-    """Download youtube video. Ensure it has a unique hour and minute timestamp , and rsync to the youtube library folder."""
-    ydl_opts = {
-        "no_warnings": True,
-        "quiet" : 1,
-        'logtostderr' : True,
-        "noprogress": False,
-        "outtmpl" : filename
-    }
-    # try:
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download(url)
-
-            # return True
-    # except Exception as e: 
-        # print(e)
-
-def get_timedelta(isoformat):
-    try :
-        s, microsecond = isoformat.split(".")
-    except :
-        s = isoformat
-        microsecond = 0
-    microsecond = min( int(microsecond) , timedelta.max.microseconds)
-    hour, minute , second =  list(map(int, s.split(":")))
-    return timedelta(minutes = minute, seconds = second, microseconds = microsecond)
-
-def draw_poses(ax, ar,  pose, default_skeleton, color):
-    data = pose.reshape(52,3)
-    x_, y_, z_ = data.T
-    artist = ax.scatter(x_, z_, y_,  s = 10, color = color)
-    ar.append(artist)
-    for origin, end in default_skeleton :
-        x, y , z   = data[end]
-        xx, yy, zz = data[origin]
-        line, = ax.plot([x, xx],[z, zz],[y,yy], color = color,  linewidth = 2)
-        ar.append(line)
-    return ax, ar
-
 class Demo :
-    """ Demo script that can be used with wether the webcam or on pats PATS """
+    """ Demo script that can be used with the webcam or on PATS """
     def __init__(self, args):
         self.args = args
         # Ground Truth pose
@@ -93,9 +53,11 @@ class Demo :
             # find filename with real extension 
             filename = list(self._downloads.glob(args.video +'*'))[0]
             # Read video & clip
-            frame_reader = VideoClipReader(filename,  get_timedelta(timecodes['start_time']), get_timedelta(timecodes['end_time'])) # src : https://stonesoup.readthedocs.io/en/v0.1b5/auto_demos/Video_Processing.html
+            frame_reader = VideoClipReader(filename, deltatime(timecodes['start_time']), deltatime(timecodes['end_time'])) # src : https://stonesoup.readthedocs.io/en/v0.1b5/auto_demos/Video_Processing.html
             self.frame_reader_ = list(frame_reader)
             video_frames_  = len(self.frame_reader_)
+        if self.count_frames == len(self.frame_reader_) - 1 :
+            plt.close()
 
 
     def start(self):
@@ -104,17 +66,16 @@ class Demo :
             self.anim = FuncAnimation(self.fig, self.animate,interval = 1, blit = False)
             plt.pause(0)
         elif self.args.from_json is not None :
-            if self.args.save :
-                self.anim = FuncAnimation(self.fig, self.animate, frames=len(self.frame_reader_), interval=1, blit = False, repeat = False)
-                writer = animation.FFMpegWriter(fps=25, metadata=dict(artist='Me'), bitrate=1800)
-                self.anim.save('./mydemo.mp4', writer = writer)
-            else :
-                self.anim = FuncAnimation(self.fig, self.animate, frames=len(self.frame_reader_), interval=1, blit = False, repeat = False)
-                plt.pause(0)
-                plt.show()
+            self.anim = FuncAnimation(self.fig, self.animate, frames=len(self.frame_reader_), interval=1, blit = False, repeat = False)
+            plt.pause(0)
+
+    def save(self) :
+        writer = animation.FFMpegWriter(fps=25, metadata=dict(artist='Me'), bitrate=1800)
+        self.anim.save('./mydemo.mp4', writer = writer)
 
     def animate(self, i):
         """ animation on each frame """
+        self.count_frames = i
         self.axis[0].clear()
         self.axis[0].set_xticks([])
         self.axis[0].set_yticks([])
@@ -138,10 +99,11 @@ class Demo :
             gt_2d  = gt[:,:2].reshape(1, 52, 2)
             predicted = self.infer_model(gt_2d)
 
-            self.axis[1], _ = draw_poses(self.axis[1], [], gt.flatten(), skeleton_connections(), color = "blue")
-            self.axis[2], _ = draw_poses(self.axis[2], [], predicted, skeleton_connections(), color = "red")
+            self.axis[1], _ = draw_skeleton(self.axis[1], [], gt.flatten(), skeleton_connections(), color = "blue")
+            self.axis[2], _ = draw_skeleton(self.axis[2], [], predicted, skeleton_connections(), color = "red")
             self.axis[1].set_title("mediapipe")
             self.axis[2].set_title("2D to 3D conversion")
+
         
     def infer_model(self, data) :
         # Convert to float32
@@ -155,6 +117,24 @@ class Demo :
         return output_data
 
     def adapt_skeleton(self, _pose3d, _handR, _handL, shape):
+        """ Convert the mediapipe skeleton to the PATS skeleton :
+            'Neck',
+            'RShoulder', 'RElbow', 'RWrist',
+            'LShoulder', 'LElbow', 'LWrist',
+            'Nose', 'REye', 'LEye',
+            'LHandRoot',
+            'LHandThumb1', 'LHandThumb2', 'LHandThumb3', 'LHandThumb4',
+            'LHandIndex1', 'LHandIndex2', 'LHandIndex3', 'LHandIndex4',
+            'LHandMiddle1', 'LHandMiddle2', 'LHandMiddle3', 'LHandMiddle4',
+            'LHandRing1', 'LHandRing2', 'LHandRing3', 'LHandRing4',
+            'LHandLittle1', 'LHandLittle2', 'LHandLittle3', 'LHandLittle4',
+            'RHandRoot',
+            'RHandThumb1', 'RHandThumb2', 'RHandThumb3', 'RHandThumb4',
+            'RHandIndex1', 'RHandIndex2', 'RHandIndex3', 'RHandIndex4',
+            'RHandMiddle1', 'RHandMiddle2', 'RHandMiddle3', 'RHandMiddle4',
+            'RHandRing1', 'RHandRing2', 'RHandRing3', 'RHandRing4',
+            'RHandLittle1', 'RHandLittle2', 'RHandLittle3', 'RHandLittle4'
+        """
         ## Tanslation to z = 0
         _handR  -= [1, 1, _handR[0,2]]
         _handL  -= [1, 1, _handL[0,2]]
@@ -166,31 +146,21 @@ class Demo :
         _handR  += [1, 1, _pose3d[16,2]]
         _handL  += [1, 1, _pose3d[15,2]]
         #  create new list
-        new_pose = []
-        #  add 'Neck'
-        new_pose.append((_pose3d[11] + _pose3d[12])/2)
-        #  add 'RShoulder', 'RElbow', 'RWrist', 
-        new_pose.append(_pose3d[12]); new_pose.append(_pose3d[14]); new_pose.append(_pose3d[16]);
-        #  add 'LShoulder', 'LElbow', 'LWrist',
-        new_pose.append(_pose3d[11]); new_pose.append(_pose3d[13]); new_pose.append(_pose3d[15]);
-        #  add 'Nose', 'REye', 'LEye'
-        new_pose.append(_pose3d[ 0]); new_pose.append(_pose3d[ 5]); new_pose.append(_pose3d[ 2]);
-        #  add 'LHand'
-        for landmark in _handL :
-            new_pose.append(landmark)
-        #  add 'RHand'
-        for landmark in _handR :
-            new_pose.append(landmark)
-
-        new_pose = np.array(new_pose)
-        return new_pose 
+        new_pose = [ (_pose3d[11] + _pose3d[12])/2, 
+                    _pose3d[12],_pose3d[14],_pose3d[16],
+                    _pose3d[11],_pose3d[13],_pose3d[15],
+                    _pose3d[ 0],_pose3d[ 5],_pose3d[ 2],
+                    *_handL, 
+                    *_handR
+                    ]
+        return np.array(new_pose)
 #
     def normalize_pose(self,pose):
         """ Normalization 
             @param : pose skeleton
         """
-        scale =  np.linalg.norm(pose[4] - pose[1])
-        return (pose - pose[0]) / scale
+        return (pose - pose[0]) / np.linalg.norm(pose[4] - pose[1])
+
 
     def get_coord(self, results):
         """ Read landmarks from mediapipe results.
@@ -245,6 +215,9 @@ class Demo :
         axis.set_xticks([])
         axis.set_yticks([])
         axis.set_zticks([])
+        axis.set_xlim(-1.5, 1.5)
+        axis.set_ylim(-1.5, 1.5)
+        axis.set_zlim(-1.5, 1.5)
 
 
 def parse_args() :
@@ -273,7 +246,6 @@ def parse_args() :
     if not (args.webcam) and not ( args.from_json and args.set and args.speaker and args.video and args.interval) :
         print('Missing args: respect order from_json, set, speaker, video, interval')
         exit()
-
     
     return args
 
@@ -292,7 +264,9 @@ if __name__ == '__main__':
     from stonesoup.reader.video import VideoClipReader
     from datetime import timedelta
     import cv2 as cv
-    from data.utils import skeleton_connections
+    from data.utils import skeleton_connections, deltatime, draw_skeleton, download_video
     # init and launch
     demo = Demo(args)
     demo.start()
+    if args.save :
+        demo.save()
